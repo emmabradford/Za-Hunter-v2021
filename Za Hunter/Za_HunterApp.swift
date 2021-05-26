847ef95676aa8c91318b260be65dabb5eefbe953
Emma Bradford <ebradford@Emmas-MacBook-Air.local>
2021-05-26 08:01:22 -0500

    finished project

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by Emma Bradford on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
